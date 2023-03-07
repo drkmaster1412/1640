3eb51869b76b727fa5e3a5837ff7fd18a46b88dc
Kojiro <truongst250402@gmail.com>
2023-03-07 21:12:09 +0700

    code (header & index) + fix (about & home)

--- about.php
+++ about.php
@@ -8,23 +8,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
+
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/style3.css">
 </head>
 
 <body>
-    <div class="about font-Ubuntu">
-        <div class="text">
-            <h1 class="center-parent">About Us</h1>
-            <p class="justify">The University of Greenwich is a public university located in London and Kent, United Kingdom. Previous names include Woolwich Polytechnic and Thames Polytechnic.</p>
-            <p class="justify"> The university's main campus is at the Old Royal Naval College, which along with its Avery Hill campus, is located in the Royal Borough of Greenwich. Greenwich also has a satellite campus in Medway, Kent, as part of a shared campus. The university's range of subjects includes architecture, business, computing, mathematics, education, engineering, humanities, maritime studies, natural sciences, pharmacy and social sciences. Greenwich's alumni include two Nobel laureates: Abiy Ahmed and Charles K. Kao. It received a Silver rating in the UK government's Teaching Excellence Framework.</p>
+    <?php echo file_get_contents("header.php"); ?>
+    <section>
+        <div class="map font-Ubuntu">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2484.6352482674924!2d-0.0081041!3d51.4832092!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487602844a443523%3A0x9f5181128b65430a!2zxJDhuqFpIGjhu41jIEdyZWVud2ljaA!5e0!3m2!1svi!2s!4v1678198012149!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-        <br>
-        <div class="text">
-            <h4>Location</h4>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2484.635247161829!2d-0.008104084577951637!3d51.48320922029315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760285263b1459%3A0x182423076697c5c8!2zVHLGsOG7nW5nIEjhuqNpIHF1w6JuIEhvw6BuZyBnaWEgY8Wp!5e0!3m2!1svi!2s!4v1678096759518!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        <div class="content font-Ubuntu">
+            <h2>About Us</h2>
+            <span><!-- line here --></span>
+
+            <p>The University of Greenwich is a public university located in London and Kent, United Kingdom.
+                Previous names include Woolwich Polytechnic and Thames Polytechnic.
+                The university's main campus is at the Old Royal Naval College, which along with its Avery Hill campus,
+                is located in the Royal Borough of Greenwich. Greenwich also has a satellite campus in Medway, Kent,
+                as part of a shared campus. The university's range of subjects includes architecture,
+                business, computing, mathematics, education, engineering, humanities, maritime studies, natural sciences,
+                pharmacy and social sciences. Greenwich's alumni include two Nobel laureates: Abiy Ahmed and Charles K. Kao.
+                It received a Silver rating in the UK government's Teaching Excellence Framework.
+            </p>
+
+            <ul class="links">
+                <li><a href="#">sign up</a></li>
+
+                <div class="vertical-line"></div>
+
+                <li><a href="#">sign in</a></li>
+
         </div>
-    </div>
+    </section>
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,14 +1,16 @@
-<?php
-    $urladmin ="http://localhost/Assignment1640/admin";
-    $homepage ="homepage.php";
-    $about ="about.php";
-    $footer = "";  
-    $urluser = "http://localhost/Assignment1640/";
-    $urllogin = "http://localhost/Assignment1640/login";
-    $signup = "signup.php";
-    $logout = "logout.php";
-    $amanage = "accountmanage.php";
-    $search = "search.php";
-     session_start();
-    include('./theme.php');
-?>
+<!-- <?php
+    // $urladmin ="http://localhost/Assignment1640/admin";
+    // $homepage ="homepage.php";
+    // $about ="about.php";
+    // $footer = "";  
+    // $urluser = "http://localhost/Assignment1640/";
+    // $urllogin = "http://localhost/Assignment1640/login";
+    // $signup = "signup.php";
+    // $logout = "logout.php";
+    // $amanage = "accountmanage.php";
+    // $search = "search.php";
+    //  session_start();
+    // include('./theme.php');
+?> -->
+<?php echo file_get_contents("header.php"); ?>
+<?php echo file_get_contents("footer.php"); ?>

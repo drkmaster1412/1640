fa7cc7ce333cfd64ad7da270359ad754c87d13af
drkmaster1412 <98210345+drkmaster1412@users.noreply.github.com>
2023-03-30 15:07:54 +0700

    Combine Branch Hoang & Phuong

--- admin/adminHeader.php
+++ admin/adminHeader.php
@@ -7,7 +7,7 @@ include('connection.php');
 <nav class="navbar navbar-expand-lg navbar-light px-5" style="background-color: #e9ecef;">
 
   <a class="logo" href="<?php echo " ?page=" . $home; ?>">
-    <img src="../image/Logo.png" align="center" width="15%" height="auto" alt="logo">
+    <img src="../image/Logo.png" text-align="center" width="15%" height="auto" alt="logo">
   </a>
 
   <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
@@ -34,7 +34,7 @@ include('connection.php');
 <nav class="navbar navbar-expand-lg navbar-light px-5" style="background-color: #e9ecef;">
 
   <a class="logo" href="<?php echo " ?page=" . $home; ?>">
-    <img src="../image/Logo.png" align="center" width="15%" height="auto" alt="logo">
+    <img src="../image/Logo.png" text-align="center" width="15%" height="auto" alt="logo">
   </a>
 
   <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>

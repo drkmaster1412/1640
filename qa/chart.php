cbd72fe079b1406ddaa4c34d4ba90dc80dcd1c66
phuong2414 <98210417+phuong2414@users.noreply.github.com>
2023-04-11 15:31:07 +0700

    css chart

--- qa/chart.php
+++ qa/chart.php
@@ -2,52 +2,55 @@
 <html>
 
 <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-        google.charts.load('current', {
-            'packages': ['bar']
-        });
-        google.charts.setOnLoadCallback(drawChart);
+    <div class="line">
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+            google.charts.load('current', { 'packages': ['corechart'] });
+            google.charts.setOnLoadCallback(drawVisualization);
 
-        function drawChart() {
-            var data = google.visualization.arrayToDataTable([
-                ['Post', 'Like ', 'Dislike'],
-                <?php
-                $query = "select * from poster";
-                $res = mysqli_query($conn, $query);
-                while ($data = mysqli_fetch_array($res)) {
-                    $p_name = $data['p_name'];
-                    $like_count = $data['like_count'];
-                    $dislike_count = $data['dislike_count'];
-                    ?>['<?php echo $p_name; ?>', <?php echo $like_count; ?>, <?php echo $dislike_count; ?>],
+            function drawVisualization() {
+                // Some raw data (not necessarily accurate)
+                // function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                    ['Post ', 'Like ', 'Dislike ', 'View '],
                     <?php
-                }
-                ?>
-            ]);
+                    $query = "select * from poster";
+                    $res = mysqli_query($conn, $query);
+                    while ($data = mysqli_fetch_array($res)) {
+                        $p_name = $data['p_name'];
+                        $like_count = $data['like_count'];
+                        $dislike_count = $data['dislike_count'];
+                        $view = $data['view'];
+                        ?>['<?php echo $p_name; ?>', <?php echo $like_count; ?>, <?php echo $dislike_count; ?>, <?php echo $view; ?>],
+                        <?php
+                    }
+                    ?>
+                ]);
 
-            var options = {
-                chart: {
+
+                var options = {
                     title: 'Post statistic',
-                    subtitle: 'Like , dislike_count',
-                },
-                bars: 'vertical' // Required for Material Bar Charts.
-            };
+                    vAxis: { title: 'Numbers' },
+                    hAxis: { title: 'Ideas' },
+                    seriesType: 'bars',
+                    series: { 2: { type: 'line' } }
+                };
 
-            var chart = new google.charts.Bar(document.getElementById('barchart_material'));
-
-            chart.draw(data, google.charts.Bar.convertOptions(options));
-        }
-    </script>
+                var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+                chart.draw(data, options);
+            }
+        </script>
+    </div>
 </head>
 
 <body>
-    <br>
-    <div id="main-content" class="container allContent-section py-6" style="margin-left:20%">
-        <div id="barchart_material" style="width: 900px; height: 500px;"></div>
-    </div>
+    <div id="chart_div" style="width: 900px; height: 500px; margin-left: 20%;"></div>
+    <div id="barchart_material_1" style="width: 800px; height: 400px; margin-left: 30%; margin-top: 5%;"></div>
 </body>
 
 </html>
+
+
 
 
 
@@ -87,7 +90,7 @@
             bars: 'vertical' // Required for Material Bar Charts.
         };
 
-        var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+        var chart = new google.charts.Bar(document.getElementById('barchart_material_1'));
 
         chart.draw(data, google.charts.Bar.convertOptions(options));
     }
